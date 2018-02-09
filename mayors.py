965fceff15fcb07ad872087d1ae3ebc859b9eb5e
Timothy Caro-Bruce <tcarobruce@gmail.com>
2018-02-08 18:04:42 -0800

    search by state, use py3, requirements.txt, better CLI

--- mayors.py
+++ mayors.py
@@ -4,37 +4,55 @@
 mayors.py - scrape information about US Mayors from usmayors.org
 """
 
-import sys
+import argparse
+import csv
+import json
 from datetime import datetime
+from os.path import splitext
+
+import requests
 from lxml import html
 
-error_message = "There are no cities or mayors with that last name"
-base_domain = "http://usmayors.org"
-base_template = "http://data.usmayors.org/database_searchID.asp?idnumber=%d"
+ALL_STATES = """
+    AL AK AZ AR CA CO CT DC DE FL GA GU HI ID IL IN IA KS KY LA ME MD MA MI
+    MN MO MP MS MT NE NV NH NJ NM NY NC ND OH OK OR PA PR RI SC SD TN TX UT
+    VT VA WA WV WI WY""".split()
 
-field_map = {
-    "next general election": "next_election",
+BASE_URL = "http://legacy.usmayors.org/"
+SEARCH_URL = "http://legacy.usmayors.org/meetmayors/mayorsatglance.asp"
+
+FIELD_MAP = {
+    "next mayoral election": "next_election",
     "mayor's e-mail address": "email",
     "city's web site": "city_site_url",
 }
 
-def get_mayor(id, uri_template=base_template):
-    url = uri_template % id 
-    doc = html.parse(url)
-    root = doc.getroot()
+CSV_FIELDS = '''
+    name email phone bio_url img_url city state population
+    city_site_url next_election'''.split()
 
-    try: 
-        if error_message in root.text_content():
-            return None
-    except Exception, e:
-        return None
-    
-    mayor_data = {'id': id}
 
-    main_table = root.cssselect('table.pagesSectionBodyTightBorder')[0]
-    main_row = main_table.getchildren()[0]
+def get_mayors_for_state(state):
+    payload = {'mode': 'search_db', 'State': state}
+    response = requests.post(SEARCH_URL, data=payload)
+    response.raise_for_status()
+    root = html.fromstring(response.content)
+
+    for node in root.cssselect('table.pagesSectionBodyTightBorder'):
+        try:
+            yield _get_mayor_from_table(node)
+        except Exception:
+            print("ERROR doing {}".format(state))
+            import traceback
+            traceback.print_exc()
+            continue
+
+
+def _get_mayor_from_table(table):
+    mayor_data = {}
+    main_row = table.getchildren()[0]
     first_cell = main_row.getchildren()[0]
-    
+
     name_and_city_state = first_cell.cssselect('strong')[0]
     [name, city_state] = name_and_city_state.xpath('text()')
     mayor_data['name'] = name
@@ -42,67 +60,80 @@ def get_mayor(id, uri_template=base_template):
     mayor_data['city'] = city
     mayor_data['state'] = state
 
-    bio_url = first_cell.cssselect('div a')
+    bio_url = first_cell.cssselect('a')
     if bio_url:
-        mayor_data['bio_url'] = bio_url[0].attrib['href'] 
+        mayor_data['bio_url'] = bio_url[0].attrib['href']
 
     for data_cell in first_cell.cssselect('table tr td'):
-        key = data_cell.text.strip().replace(':','').lower()
-        key = field_map.get(key, key)
-        value_cell = data_cell.cssselect('b')
-        if not value_cell:
-            value_cell = data_cell.cssselect('a')
-        if value_cell:
-            val = value_cell[0].text_content().strip()
-            mayor_data[key] = val
+        key, value = data_cell.text_content().split(':', 1)
+        key = key.strip().lower()
+        key = FIELD_MAP.get(key, key)
+        mayor_data[key] = value.strip()
 
     mayor_data['population'] = mayor_data.get('population', '').replace(',', '')
 
+    mayor_data['email'] = mayor_data.get('email', '').replace('mailto:', '')
+
     next_election = mayor_data.get('next_election')
     if next_election:
-        parsed_next_election = datetime.strptime(next_election, "%m/%d/%Y") 
-        mayor_data['next_election'] = parsed_next_election.strftime("%Y-%m-%d") 
+        try:
+            parsed_next_election = datetime.strptime(next_election, "%m/%d/%Y")
+            mayor_data['next_election'] = parsed_next_election.strftime("%Y-%m-%d")
+        except ValueError:
+            pass
 
-    img_path = main_row.getchildren()[2].cssselect('img')[0].attrib['src']    
-    mayor_data['img_url'] = img_path
-
-    for k, v in mayor_data.items():
-        if hasattr(v, 'encode'):
-            mayor_data[k] = v.encode('utf-8')
+    img_path = main_row.getchildren()[2].cssselect('img')[0].attrib['src']
+    mayor_data['img_url'] = BASE_URL + img_path.lstrip('/')
 
     return mayor_data
 
-def all_mayors(ids=range(1,4000), uri_template=base_template):
-    for i in ids:
-        data = None
-        try: 
-            data = get_mayor(i, uri_template)
-        except Exception, e:
-            sys.stderr.write("Error on %d %s\n" % (i, str(e)))
 
-        if data:
-            yield data
+def get_mayors(states=ALL_STATES):
+    for state in states:
+        for mayor in get_mayors_for_state(state):
+            yield mayor
 
-def write_all_to_csv(out=None, ids=range(1,4000), uri_template=base_template):
-    import csv 
-    if out is None:
-        out = sys.stdout
-    
-    fields = 'id,name,email,phone,bio_url,img_url,city,state,population,city_site_url,next_election'.split(',')
 
-    w = csv.DictWriter(out, fields)
-    for mayor in all_mayors(ids=ids, uri_template=uri_template):
+def write_to_csv(mayors, out):
+    w = csv.DictWriter(out, CSV_FIELDS)
+    w.writeheader()
+    for mayor in mayors:
         w.writerow(mayor)
 
-def write_all_to_json(out=None, ids=range(1,4000), uri_template=base_template):
-    import json 
-    if out is None:
-        out = sys.stdout
-    mayors = []
-    for mayor in all_mayors(ids=ids, uri_template=uri_template):
-        mayors.append(mayor)
-    json.dump(mayors, out, indent=4)
+
+def write_to_json(mayors, out):
+    json.dump(list(mayors), out, indent=4)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Scrape US mayoral data from usmayors.org")
+
+    parser.add_argument('out', type=argparse.FileType('w', encoding="UTF-8"),
+                        default='-')
+    parser.add_argument('--format', choices=['csv', 'json'])
+    parser.add_argument('--state', nargs='*', default=ALL_STATES)
+
+    args = parser.parse_args()
+
+    # guess format from file extension
+    if args.format is None:
+        fn = args.out.name
+        if fn != '<stdout>':
+            _, ext = splitext(fn)
+            args.format = ext[1:]
+        else:
+            args.format = 'csv'
+
+    args.writer = {
+        'csv': write_to_csv,
+        'json': write_to_json,
+    }[args.format]  # may KeyError if format is unrecognized
+
+    return args
+
 
 if __name__ == '__main__':
-    write_all_to_csv()
-
+    args = parse_arguments()
+    mayors = get_mayors(states=args.state)
+    args.writer(mayors, args.out)
